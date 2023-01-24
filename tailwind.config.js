84bd424588d406c10278641e7e4ddd7f1b2db410
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-24 18:35:04 +0200

    config. tailwind, additional colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        _blue: {
+          300: 'hsl(228, 39%, 23%)',
+          500: 'hsl(227, 12%, 61%)',
+          700: 'hsl(233, 12%, 13%)',
+        },
+        _red: {
+          300: 'hsl(12, 88%, 59%)',
+          400: 'hsl(13, 100%, 96%)',
+        },
+        _gray: {
+          300: 'hsl(0, 0%, 98%)',
+        }
+      }
+    },
   },
   plugins: [],
 }
