40ecd260116342600062ea66dd070f29927a3ce7
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-24 17:18:54 +0200

    BASIC

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+'use client'
+import Head from 'next/head'
+import Image from 'next/image'
+import { useEffect } from 'react'
+import { Navbar }  from '../components/Navbar'
+import googleIcon from '../public/google.png'
+
+
+
+export default function Home() {
+
+
+
+  return (
+    <div className="">
+      MANAGE LANDING PAGE
+    </div>
+  )
+}
