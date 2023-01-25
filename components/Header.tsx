af4b29b9e94a4d628155e8b2178a0d8c1e0d820f
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-25 15:25:09 +0200

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,24 @@
-import { Component } from 'react'
+import React from 'react'
 import Image from 'next/image'
 import Logo from '../public/images/logo.svg'
 import Hamburger from '../public/images/icon-hamburger.svg'
 import BgTabletPattern from '../public/images/bg-tablet-pattern.svg'
 
-export const Header: Component = () => {
+export const Header: React.FC = () => {
 
 	return (
 		<header className=" py-12 flex justify-between">
-			<Image className="fixed w-[200px]" src={Logo}/>
+			<Image alt={`Manage's Logo`} className="fixed w-[200px]" src={Logo}/>
 			<nav className="none">
 				
 			</nav>
 			<Image
         className="fixed -top-[90px] -right-[90px] back"
+				alt=''
         src={BgTabletPattern}
       />
       <button className="fixed right-10">
-				<Image className=" w-10 " src={Hamburger}/>
+				<Image className=" w-10 " alt='Mobile Menu' src={Hamburger}/>
 			</button>
 		</header>
 	)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,26 @@
 'use client'
 import Head from 'next/head'
 import Image from 'next/image'
-import { useEffect } from 'react'
 import { Header }  from '../components/Header'
+import { Title } from "../components/Title"
 import IllustrationIntro from '../public/images/illustration-intro.svg'
+import favicon from '../public/images/favicon-32x32.png'
 
 
 export default function Home() {
 
-
-
   return (
     <div className="">
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" type="image/png" sizes="32x32" href={favicon.src}/>
+        <title>Frontend Mentor | Manage landing page</title>
+      </Head>
       <Header  />
       <section id="intro" className="sm:w-[100%]">
-        <Image src={IllustrationIntro}/>
+        <Image alt='An Illustration Intro to Manage' src={IllustrationIntro}/>
+        <Title>Bring everyone together to build better products.</Title>
       </section>
     </div>
   )
