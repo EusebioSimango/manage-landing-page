aab0b0e0d809335b8e4ffc0909f0f55450c00392
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-25 19:15:02 +0200

    create GetStartedButton Component

---
+++ components/GetStartedButton.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+export const GetStartedButton: React.FC = () => {
+	return (
+		<div className="w-[100%] flex justify-center mb-4">
+			<button 
+				className="text-white bg-Red-500 py-3 px-6 text-sm font-medium rounded-[50px] shadow-md"
+			>
+				Get Started
+			</button>
+		</div>
+	)
+}
