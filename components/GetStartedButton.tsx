ff1bc0f473de2f04f1492163a52dfd5610daaf3e
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-25 19:48:16 +0200

    fixing scroll, and make the header fixed

--- components/GetStartedButton.tsx
+++ components/GetStartedButton.tsx
@@ -2,7 +2,7 @@ import React, { ReactNode } from 'react'
 
 export const GetStartedButton: React.FC = () => {
 	return (
-		<div className="w-[100%] flex justify-center mb-4">
+		<div className="w-[100%] flex justify-center mb-10">
 			<button 
 				className="text-white bg-Red-500 py-3 px-6 text-sm font-medium rounded-[50px] shadow-md"
 			>
--- pages/index.tsx
+++ pages/index.tsx
@@ -19,13 +19,21 @@ export default function Home() {
         <title>Frontend Mentor | Manage landing page</title>
       </Head>
       <Header  />
-      <section id="intro" className="sm:w-[100%]">
+      <main className="fixed right-0 px-9 w-[100vw] max-h-[calc(100vh-24px)] top-24 overflow-y-auto back">
+        <section id="intro" className="sm:w-[100%]">
         <Image alt='An Illustration Intro to Manage' src={IllustrationIntro}/>
         <Title>Bring everyone together to build better products.</Title>
         <Paragraph>Manage makes it simple for software teams to plan day-to-day 
   tasks while keeping the larger team goals in view.</Paragraph>
         <GetStartedButton />
-      </section>
+        </section>
+        <section>
+          <Title>What’s different about Manage?</Title>
+          <Paragraph>Manage provides all the functionality your team needs, without 
+    the complexity. Our software is tailor-made for modern digital 
+    product teams. </Paragraph>
+        </section>
+      </main>
     </div>
   )
 }
