a53ee5b908bfd7ed25e237bd5d7d98fd7dc76c94
Eusebio Simango <eusebiosimango14@gmail.com>
2023-01-25 18:42:16 +0200

    rename component file from Title.tsx to Text.tsx

---
+++ components/Text.tsx
@@ -0,0 +1,17 @@
+import React, { ReactNode } from 'react'
+
+interface IProps  {
+	children: ReactNode;
+}
+
+export const Title: React.FC<IProps> = ({ children })  => {
+	return (
+		<h1 className="text-Blue-500 leading-[1.3] font-medium text-6xl text-center">
+			{ children }
+		</h1>
+	)
+}
+
+export const Paragraph: React.FC<IProps> = ({ children }) => {
+	return <p className="text-Blue-300 text-center leading-[1.3] text-3xl font-regular py-6">{ children }</p>
+}
